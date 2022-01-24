ae1c3b136e042941ba5617645493a06db90d89e6
rashmi1120 <rashmi.bsubash@gmail.com>
2022-01-24 17:21:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>CSS Ripped effect</h1>
+
+        <div className="ripped-effect">
+          <div class="bottom-ripped">
+            <article>
+              <p>
+                Playing with svg image to get the ripped page look. Change the
+                background-size to change the rippy look.
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
